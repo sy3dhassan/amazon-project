57ef1d5b1130a4c6f793d27551804b5c6dbe399a
sy3dhassan <syedhassanzia08@gmail.com>
2025-04-25 15:50:05 +0500

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -79,6 +79,14 @@ button.addEventListener('click', () => {
         quantity: 1
     });
   };
-  console.log(cart)
+
+  let cartQuantity = 0;
+
+  cart.forEach((item) => {
+    cartQuantity += item.quantity;
+  })
+
+  document.querySelector('.js-cart-quantity')
+  .innerHTML = cartQuantity;
 });
 });
